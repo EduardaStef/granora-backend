648b472856adb7a845e69c5ce9b369f56805fac9
eduarda.steffen <eduarda.steffen@hotmail.com>
2026-08-31 01:55:06 -0300

    ajuste checkstyle

--- src/main/java/com/devduda/granora/service/UsuarioService.java
+++ src/main/java/com/devduda/granora/service/UsuarioService.java
@@ -8,9 +8,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import java.util.Objects;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService implements UserDetailsService {
